a28f56a4ff9b700bb158e14e9a87595c089d8667
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-05-01 09:34:29 +0200

    cleaned up seq logger

--- src/Infrastructure/Logger/logger.ts
+++ src/Infrastructure/Logger/logger.ts
@@ -6,10 +6,18 @@ const transports = [
     new SeqTransport({
         serverUrl: process.env.SEQ_URL || "http://seq:5341",
         onError: (e => { console.error(e) }),
+        handleExceptions: true,
+        handleRejections: true,
     })
 ]
 
 const Logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(  /* This is required to get errors to log with stack traces. See https://github.com/winstonjs/winston/issues/1498 */
+        winston.format.errors({ stack: true }),
+        winston.format.json(),
+    ),
+    defaultMeta: {application: "webapi hestia"},
     transports,
 })
 
