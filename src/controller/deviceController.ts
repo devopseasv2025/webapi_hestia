1e2a33d6003fbb0405b91d1d46f5bcc129fdb0a1
eliastherkildsen <elias3004@outllook.dk>
2025-05-04 14:01:47 +0200

    Add error handling and type annotations in device controller
    Introduced try-catch blocks to handle errors in methods for fetching devices, ensuring proper logging and consistent error responses. Added type annotations for better type safety and code clarity.

--- src/controller/deviceController.ts
+++ src/controller/deviceController.ts
@@ -1,7 +1,7 @@
 import {IDeviceRepository} from "../Repository/IDeviceRepository.js";
 import {RegexUtils} from "../Utilities/regexUtils.js";
 import Logger from "../Infrastructure/Logger/logger.js";
-
+import {IDevice} from "../Entities/Interfaces/IDevice";
 
 interface IDeviceController {
     getDeviceByMac(req: any, res: any): Promise<Response>;
@@ -20,34 +20,47 @@ export default class DeviceController implements IDeviceController {
     }
 
     public async getDeviceById(req: any, res: any): Promise<Response> {
-        if (!req.params?.id) {
-            return res.status(404).send("Id missing required field");
-        }
 
-        const device = await this._deviceRepository.readDeviceById(req.params.id);
-        return await res.status(200).send(device);
+        try {
+
+            if (!req.params?.id) {
+                return res.status(404).send("Id missing required field");
+            }
+
+            const device : IDevice = await this._deviceRepository.readDeviceById(req.params.id);
+            return await res.status(200).send(device);
+
+        } catch (err) {
+            Logger.error("Error fetching device by id: ", err);
+            return res.status(500).send("Internal server error");
+        }
 
     }
 
-    public async getDeviceByMac(req: any, res: any) {
+    public async getDeviceByMac(req: any, res: any): Promise<Response> {
+        try {
+            if (!req.params?.mac) {
+                return res.status(400).send("Device ID is required");
+            }
 
-        if (!req.params?.mac) {
-            return res.status(400).send("Device ID is required");
+            // Check if the device ID is a valid mac address
+            if (!RegexUtils.isValidMacAddress(req.params?.mac)) {
+                return res.status(400).send("Invalid Device ID, must be a valid mac address for the device");
+            }
+
+            const device : IDevice = await this._deviceRepository.readDeviceByMacAddress(req.params.mac);
+            return await res.status(200).send(device);
+        } catch (err) {
+            Logger.error("Error fetching device by mac address: ", err);
+            return res.status(500).send("Internal server error");
         }
 
-        // Check if the device ID is a valid mac address
-        if (!RegexUtils.isValidMacAddress(req.params?.mac)) {
-            return res.status(400).send("Invalid Device ID, must be a valid mac address for the device");
-        }
-
-        const device = await this._deviceRepository.readDeviceByMacAddress(req.params.mac);
-        return await res.status(200).send(device);
     }
 
     public async getDevices(req: any, res: any): Promise<Response> {
 
         try {
-            const devices = await this._deviceRepository.readAllDevices();
+            const devices : IDevice[] = await this._deviceRepository.readAllDevices();
 
             if (devices.length === 0) {
                 return res.status(404).send("No devices found");
