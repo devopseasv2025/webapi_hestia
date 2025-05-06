70507f770e1f1f18e84d4ddee8356a60fa85cbba
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-05-06 10:01:10 +0200

    Update workflow to use `docker compose` and simplify cleanup
    Replaced `docker-compose` with the updated `docker compose` command for consistency with modern Docker CLI usage. Simplified the container cleanup step by using `docker compose down` instead of individual stop and remove commands.

--- k6/scripts/spike_test.js
+++ k6/scripts/spike_test.js
@@ -6,17 +6,16 @@ export let options = {
     stages: [
         { duration: '10s', target: 100 },
         { duration: '1m', target: 100 },
-        // { duration: '10s', target: 1400 },
-        // { duration: '3m', target: 1400 },
-        // { duration: '10s', target: 100 },
-        // { duration: '3m', target: 100 },
-        // { duration: '10s', target: 0 },
+        { duration: '10s', target: 1400 },
+        { duration: '3m', target: 1400 },
+        { duration: '10s', target: 100 },
+        { duration: '3m', target: 100 },
+        { duration: '10s', target: 0 },
     ],
 
 
 
     thresholds: {
-        http_req_failed: ['rate<0.001'], // http errors should be less than 0.1%
         http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
     },
 
