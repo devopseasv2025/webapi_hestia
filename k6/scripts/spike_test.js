2aab8c1c9191f897f180b4dd3268612281c8e156
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2025-05-06 11:04:45 +0200

    Update spike test stages and use const for options
    Modified the load test stages to adjust ramp-up and duration for better testing granularity. Replaced `let` with `const` for the `options` object to enforce immutability and maintain consistent coding practices.

--- k6/scripts/spike_test.js
+++ k6/scripts/spike_test.js
@@ -1,16 +1,14 @@
 import http from 'k6/http';
 
-export let options = {
+export const options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
     stages: [
-        { duration: '10s', target: 100 },
-        // { duration: '1m', target: 100 },
-        // { duration: '10s', target: 1400 },
-        // { duration: '3m', target: 1400 },
-        // { duration: '10s', target: 100 },
-        // { duration: '3m', target: 100 },
-        // { duration: '10s', target: 0 },
+        { duration: '30s', target: 50 },
+        { duration: '1m', target: 50 },
+        { duration: '30s', target: 100 },
+        { duration: '2m', target: 100 },
+        { duration: '30s', target: 0 },
     ],
 
 
